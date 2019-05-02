ce8878111900576bd30c57def7c36d5a3d2a338a
ksarapultsev <ksarapultsev2014@gmail.com>
2019-05-02 15:49:39 +0600

    Добавлена сортировка пузырьком

---
+++ Algotithm/AlgorithmBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algotithm
+{
+    public class AlgorithmBase<T> where T: IComparable
+    {
+        public List<T> Items { get; set; } = new List<T>();
+
+        protected void Swop(int positionA, int positionB)
+        {
+            if (positionA < Items.Count && positionB < Items.Count)
+            {
+                var temp = Items[positionA];
+                Items[positionA] = Items[positionB];
+                Items[positionB] = temp;
+            }
+        }
+
+      
+        public virtual void Sort()
+        {
+            Items.Sort();
+        }
+    }
+
+}
--- SortAlgorithms/Form1.cs
+++ SortAlgorithms/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algotithm;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,30 @@ namespace SortAlgorithms
 {
     public partial class Form1 : Form
     {
+        AlgorithmBase<int> algotithm = new BubbleSort<int>();
+
         public Form1()
         {
             InitializeComponent();
+        }       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(textBox1.Text, out int value))
+            {
+                algotithm.Items.Add(value);
+                label1.Text += " " + value;
+            }
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            algotithm.Sort();
+            foreach (var item in algotithm.Items)
+            {
+                label2.Text += " " + item;
+            }
         }
     }
 }
